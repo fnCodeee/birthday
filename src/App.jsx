ffff3a1678db436de4f4f4c873db67df03963572
fnCodeee <faizna@gmail.com>
2024-12-12 18:39:06 +0700

    deploy birthday

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card.jsx";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY Love!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY NAYNAYY!</h3>
+        <p>Dear Sengkuu,</p>
+        <p>
+        Selamat ulang tahun sayangku!! Semoga hari-harimu dipenuhi dengan banyak cinta dan tawa!, jadi anak yang sholehah dan semoga semua harapan ulang tahunmu menjadi kenyataan.
+        </p>
+        <p className="name">Faiz</p>
+      </div>
+    </div>
+  );
+  
